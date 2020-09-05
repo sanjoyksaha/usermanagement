1e0e9ef0cf58be3c4c0efe30455fda8f7f45ca5b
Sanjoy Kumar Saha <sanjoyksaha0@gmail.com>
2020-09-05 21:16:57 +0600

    email verification added

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes([
+    'verify' => true
+]);
 
 Route::get('/home', 'HomeController@index')->name('home');
 
